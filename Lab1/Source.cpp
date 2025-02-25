b6acd9e3fd985060b31c685992173b3679604d66
Ghiorghies Andrei-Cristian <andrey5015n@gmail.com>
2025-02-25 19:53:11 +0200

    Prima problema

--- Lab1/Source.cpp
+++ Lab1/Source.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+int atoi_custom(char s[100]) {
+	int nr = 0;
+	for (int i = 0; i < 100 && s[i]; i++)
+		if(s[i] >= '0' && s[i] <= '9')
+			nr = nr * 10 + s[i] - '0';
+	return nr;
+}
+
 int main() {
-	std::cout << "Hello";
+	FILE* file;
+	int suma = 0;
+	char buffer[100];
+
+	fopen_s(&file, "in.txt", "r");
+
+	while (fgets(buffer, 100, file))
+		suma += atoi_custom(buffer);
+	
+	printf("%d", suma);
+
 	return 0;
 }
